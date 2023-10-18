8f8c0aa7750cfb65d89286d103ca41bd7c99d6ea
jkaiser <jayckaiser@gmail.com>
2023-10-18 11:59:21 -0500

    Refactor retry logic in async classes; general cleanup.

--- edfi_api_client/async_mixin.py
+++ edfi_api_client/async_mixin.py
@@ -1,16 +1,15 @@
 import aiofiles
 import aiohttp
+import aiohttp_retry
 import asyncio
 import functools
 import itertools
 import logging
 
-from requests.exceptions import RequestsWarning
-
 from edfi_api_client import util
 from edfi_api_client.session import EdFiSession
 
-from typing import Awaitable, AsyncGenerator, Callable, List, Optional
+from typing import Awaitable, AsyncGenerator, Callable, List, Optional, Set
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from edfi_api_client import EdFiClient
@@ -21,6 +20,8 @@ class AsyncEdFiSession(EdFiSession):
     """
 
     """
+    retry_status_codes: Set[int] = {401, 500, 504}
+
     def __init__(self, *args, pool_size: int = 8, **kwargs):
         super().__init__(*args, pool_size=pool_size, **kwargs)
         self.pool_size = pool_size
@@ -29,7 +30,6 @@ class AsyncEdFiSession(EdFiSession):
             logging.warning("Client key and secret not provided. Async connection with ODS will not be attempted.")
             exit(1)
 
-        self.auth_headers = {}
         self.session: aiohttp.ClientSession = None
 
     async def __aenter__(self):
@@ -38,13 +38,32 @@ class AsyncEdFiSession(EdFiSession):
     async def __aexit__(self, *exc):
         return await self.session.close()
 
-    async def connect(self, **kwargs) -> 'AsyncEdFiSession':
+    async def connect(self,
+        retry_on_failure: bool = False,
+        max_retries: int = 5,
+        max_wait: int = 500,
+        **kwargs
+    ) -> 'AsyncEdFiSession':
+        # Initialize a session if not yet initialized.
         if self.session is None:
-            self.session = aiohttp.ClientSession(raise_for_status=True, **kwargs)
+            self.session = aiohttp.ClientSession(
+                connector = aiohttp.connector.TCPConnector(limit=self.pool_size),
+                timeout=aiohttp.ClientTimeout(sock_connect=max_wait),
+                raise_for_status=True,
+                **kwargs
+            )
 
-        # Updates time attributes to match response
-        auth_info = self.get_auth_response().json()
-        access_token = auth_info['access_token']
+            if retry_on_failure:
+                retry_options = aiohttp_retry.ExponentialRetry(
+                    attempts=max_retries,
+                    max_timeout=max_wait,
+                    statuses=self.retry_status_codes
+                )
+
+                self.session = aiohttp_retry.RetryClient(
+                    client_session=self.session,
+                    retry_options=retry_options
+                )
 
         # Update time attributes and auth headers with latest authentication information.
         self.authenticate()
@@ -65,8 +84,6 @@ class AsyncEdFiSession(EdFiSession):
     ) -> aiohttp.ClientResponse:
         """
         Complete a GET request against an endpoint URL.
-        TODO: Process not cancelled upon error.
-        TODO: Reauthentication unsuccessful.
 
         :param url:
         :param params:
@@ -77,9 +94,6 @@ class AsyncEdFiSession(EdFiSession):
         """
         self.refresh_if_expired()
 
-        if retry_on_failure:
-            return await self.get_response_with_exponential_backoff(url, params, max_retries=max_retries, max_wait=max_wait, **kwargs)
-
         async with self.session.get(
             url, headers=self.auth_headers, params=params,
             verify_ssl=self.verify_ssl, raise_for_status=False
@@ -87,44 +101,6 @@ class AsyncEdFiSession(EdFiSession):
             self.custom_raise_for_status(response)
             text = await response.text()
             return response
-
-    async def get_response_with_exponential_backoff(self,
-        url: str,
-        params: 'EdFiParams',
-        *,
-        max_retries: int = 5,
-        max_wait: int = 600,
-        **kwargs
-    ) -> aiohttp.ClientResponse:
-        """
-        Complete a GET request against an endpoint URL.
-        In the case of failure, retry with exponential backoff until max_retries or max_wait has been exceeded.
-
-        :param url:
-        :param params:
-        :param max_retries:
-        :param max_wait:
-        :return:
-        """
-        # Attempt the GET until success or `max_retries` reached.
-        for n_tries in range(max_retries):
-
-            try:
-                return await self.get_response(url, params, **kwargs)
-
-            except RequestsWarning:
-                # If an API call fails, it may be due to rate-limiting.
-                # Use exponential backoff to wait, then refresh and try again.
-                await asyncio.sleep(
-                    min((2 ** n_tries) * 2, max_wait)
-                )
-                logging.warning(f"Retry number: {n_tries}")
-
-        # This block is reached only if max_retries has been reached.
-        else:
-            logging.warning(f"[Get with Retry Failed] Endpoint  : {url}")
-            logging.warning(f"[Get with Retry Failed] Parameters: {params}")
-            raise RuntimeError("API GET failed: max retries exceeded for URL.")
 
     async def get_total_count(self, url: str, params: 'EdFiParams', **kwargs) -> int:
         """
@@ -164,6 +140,9 @@ class AsyncEndpointMixin:
         def wrapped(self,
             *args,
             session: Optional['AsyncEdFiSession'] = None,
+            retry_on_failure: bool = False,
+            max_retries: int = 5,
+            max_wait: int = 500,
             **kwargs
         ):
             if session:
@@ -171,7 +150,11 @@ class AsyncEndpointMixin:
 
             # Otherwise, build the connection and complete a full asyncio run.
             async def main():
-                async with await self.client.async_session.connect() as session:
+                async with await self.client.async_session.connect(
+                    retry_on_failure=retry_on_failure,
+                    max_retries=max_retries,
+                    max_wait=max_wait
+                ) as session:
                     return await func(self, *args, session=session, **kwargs)
 
             return asyncio.run(main())
@@ -214,7 +197,6 @@ class AsyncEndpointMixin:
                 retry_on_failure=retry_on_failure, max_retries=max_retries, max_wait=max_wait
             )
             return await res.json()
-
 
         self.client.verbose_log(f"[Async Paged Get {self.type}] Endpoint  : {self.url}")
 
@@ -279,7 +261,6 @@ class AsyncEndpointMixin:
             step_change_version=step_change_version, change_version_step_size=change_version_step_size, reverse_paging=reverse_paging
         )
 
-        # TODO: The file isn't being created upon 'open'.
         async with aiofiles.open(path, 'wb') as fp:
             await self.gather_with_concurrency(
                 session.pool_size,
@@ -295,7 +276,7 @@ class AsyncEndpointMixin:
         step_change_version: bool,
         change_version_step_size: int,
         reverse_paging: bool
-    ) -> Awaitable[List['EdFiParams']]:
+    ) -> List['EdFiParams']:
         """
 
         :param session:
@@ -308,7 +289,6 @@ class AsyncEndpointMixin:
         async def build_total_count_windows(params):
             total_count = await session.get_total_count(self.url, params)
             return params.build_offset_window_params(page_size, total_count=total_count, reverse=reverse_paging)
-
 
         if step_change_version:
             top_level_params = self.params.build_change_version_window_params(change_version_step_size)
