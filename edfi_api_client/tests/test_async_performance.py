0dfc040a47855114629c2f651ae7bb0abeef1708
jayckaiser <jayckaiser@gmail.com>
2024-02-16 08:34:52 -0600

    Clean up async_performance test with new counts for Cougar 2024.

--- edfi_api_client/tests/test_async_performance.py
+++ edfi_api_client/tests/test_async_performance.py
@@ -8,26 +8,6 @@ from edfi_api_client import EdFiClient
 from test_util import format_print, time_it
 
 ###
-def time_it(func, wrap_func=None, *args, **kwargs):
-    """
-    Helper for retrieving runtime of a function.
-    Return runtime (in seconds), followed by function return.
-
-    ```
-    runtime, return_value = time_it(function, arguments)
-    ```
-
-    :return:
-    """
-    start = time.time()
-    return_val = func(*args, **kwargs)
-    if wrap_func:
-        return_val = wrap_func(return_val)
-    end = time.time()
-
-    runtime = round(end - start, 2)
-    return runtime, return_val
-
 def test_async(secret: str, verbose: bool = False):
     """
 
@@ -38,20 +18,20 @@ def test_async(secret: str, verbose: bool = False):
     credentials = easecret.get_secret(secret)
     edfi = EdFiClient(**credentials, verbose=verbose)
 
-    # Map resources to exact change versions to extract N * 1000 rows.
+    # Map resources to estimate change versions to extract N * 1000 rows.
     # Source: `edfi_scde_2023`
     max_change_versions = {
         "students": {
-            20 : 491831,
-            # 40 : 945701,
+            20 : 2725200,  # TODO: Re-find these counts.
+            40 : 3650700,
             # 80 : 1596723,
             # 160: 2928422,
             # 320: 4649473,
             # 640: 11579986,
         },
         "studentSectionAttendanceEvents": {
-            20 : 70738320,
-            # 40 : 70825225,
+            20 : 217100,
+            40 : 2250700,
             # 80 : 70926490,
             # 160: 71174025,
             # 320: 72151990,
@@ -77,7 +57,7 @@ def test_async(secret: str, verbose: bool = False):
         for k_row_count, max_change_version in cv_row_counts.items():
 
             endpoint = edfi.resource(resource, minChangeVersion=0, max_change_version=max_change_version)
-            endpoint_count = endpoint.total_count
+            endpoint_count = endpoint.get_total_count()
 
 
             ## Synchronous Pull
@@ -97,20 +77,18 @@ def test_async(secret: str, verbose: bool = False):
             for pool_size in pool_sizes:
                 async_kwargs.update(pool_size=pool_size)
 
-                print(f"\nResource: {resource}; Num rows: {k_row_count}k; Pool size: {pool_size}")
-                runtime, rows = time_it(endpoint.async_get_rows, **async_kwargs)
-
                 # Get row count of written file.
-                if len(rows) != endpoint_count:
-                    print( "    Number of extracted rows did not match:")
-                    print(f"    Expected: {endpoint_count} ; Pulled: {len(rows)}")
+                print(f"\nResource: {resource}; Num rows: {k_row_count}k; Pool size: {pool_size}")
+                runtime, resource_output_path = time_it(endpoint.async_get_to_json, **async_kwargs)
+
+                with open(resource_output_path, 'r') as fp:
+                    assert len(fp.readlines()) == endpoint_count
 
                 print(f"    Runtime: {runtime} seconds")
 
 
 if __name__ == '__main__':
-    # MASTER_SECRET = "edfi_sc_cougar_2024"
-    MASTER_SECRET = "edfi_scde_2023"
+    MASTER_SECRET = "edfi_sc_cougar_2024"
     VERBOSE = False
 
     test_async(MASTER_SECRET, verbose=VERBOSE)
