de0ade6fabe37d1afc54be53e96e0ea0f12fa4df
jayckaiser <jayckaiser@gmail.com>
2024-04-04 18:31:47 -0500

    Re-add setup.py (where did it go?)

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+import setuptools
+
+VERSION = "0.3.0"
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+setuptools.setup(
+      name='edfi_api_client',
+      version=VERSION,
+      description='Ed-Fi API client and tools',
+      license_files=['LICENSE'],
+      url='https://github.com/edanalytics/edfi_api_client',
+
+      author='Erik Joranlien, Jay Kaiser',
+      author_email='ejoranlien@edanalytics.org, jkaiser@edanalytics.org',
+
+      long_description=README,
+      long_description_content_type='text/markdown',
+      keyword='edfi, ed-fi, api, client, data',
+
+      packages=['edfi_api_client'],
+      install_requires=['requests'],
+      extras_require = {
+            'async': ['aiohttp', 'aiohttp_retry', 'aiofiles']
+      },
+      zip_safe=False,
+)
