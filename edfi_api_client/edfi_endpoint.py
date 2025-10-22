5b2f79d383541bd7aedd40c7439fe67b0daadf2f
jayckaiser <jayckaiser@gmail.com>
2025-10-22 11:40:24 -0500

    Update get_rows/pages docstrings and reorder arguments for clarity.

--- edfi_api_client/edfi_endpoint.py
+++ edfi_api_client/edfi_endpoint.py
@@ -153,7 +153,11 @@ class EdFiEndpoint:
         This method returns all rows from an endpoint, applying pagination logic as necessary.
         Rows are returned as a generator.
 
+        :param params:
         :param page_size:
+        :param reverse_paging:
+        :param step_change_version:
+        :param change_version_step_size:
         :param retry_on_failure:
         :param max_retries:
         :param max_wait:
@@ -174,9 +178,9 @@ class EdFiEndpoint:
         *,
         params: Optional[dict] = None,  # Optional alternative params
         page_size: int = 100,
+        reverse_paging: bool = True,
         step_change_version: bool = False,
         change_version_step_size: int = 50000,
-        reverse_paging: bool = True,
         **kwargs
     ) -> Iterator[List[dict]]:
         """
@@ -184,9 +188,13 @@ class EdFiEndpoint:
         Rows are returned as a generator.
 
         :param params:
+        :param page_size:
+        :param reverse_paging:
         :param step_change_version:
         :param change_version_step_size:
-        :param reverse_paging:
+        :param retry_on_failure:
+        :param max_retries:
+        :param max_wait:
         :return:
         """
         # Override init params if passed
