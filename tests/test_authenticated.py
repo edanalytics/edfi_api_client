e477ddb5260b678a761f519c4431bc6a7ce1e513
Alex Chen <achen@edanalytics.org>
2025-12-10 13:39:34 -0800

    Add mocked test for default uncached client fetching own token

--- tests/test_authenticated.py
+++ tests/test_authenticated.py
@@ -1,5 +1,6 @@
 from edfi_api_client import EdFiClient
 
+import pytest
 from requests.auth import _basic_auth_str
 import responses
 from responses import matchers
@@ -12,10 +13,11 @@ BASIC_AUTH_HEADER = _basic_auth_str(CLIENT_KEY, CLIENT_SECRET)
 TOKEN = 'token'
 INSTANCE_CODE = 'instance_code'
 
-@responses.activate
-def test_get_token_info():
-    """Test that client.get_token_info() POSTs with a valid token from auth"""
-    responses.get(
+
+@pytest.fixture
+def base_url_response():
+    """Mock response to root endpoint; clients will hit this to get apiMode"""
+    return responses.get(
         BASE_URL,
         json={
             'version': '7.1',
@@ -35,6 +37,11 @@ def test_get_token_info():
             }
         }
     )
+
+
+@responses.activate
+def test_get_token_info(base_url_response):
+    """Test that client.get_token_info() POSTs with a valid token from auth"""
     responses.post(
         f'{BASE_URL}/oauth/token',
         json={
@@ -74,28 +81,8 @@ def test_get_token_info():
     assert(token_info.get('client_id') == CLIENT_KEY)
 
 @responses.activate
-def test_instance_specific_connect():
+def test_instance_specific_connect(base_url_response):
     """Test that a client started in instance-year specific mode hits the right oauth/token URL"""
-    responses.get(
-        BASE_URL,
-        json={
-            'version': '7.1',
-            'informationalVersion': '7.1',
-            'suite': '3',
-            'build': '2025.5.1.1636',
-            'apiMode': 'Instance-Year Specific',
-            'dataModels': [{'informationalVersion': 'The Ed-Fi Data Model 5.0',
-                 'name': 'Ed-Fi',
-                 'version': '5.0.0'}],
-            'urls': {
-                'dependencies': f'{BASE_URL}/metadata/data/v3/dependencies',
-                'openApiMetadata': f'{BASE_URL}/metadata/',
-                'oauth': f'{BASE_URL}/oauth/token',
-                'dataManagementApi': f'{BASE_URL}/data/v3/',
-                'xsdMetadata': f'{BASE_URL}/metadata/xsd'
-            }
-        }
-    )
     responses.post(
         f'{BASE_URL}/{INSTANCE_CODE}/oauth/token',
         json={
@@ -111,4 +98,37 @@ def test_instance_specific_connect():
     client = EdFiClient(BASE_URL, CLIENT_KEY, CLIENT_SECRET, api_mode='instance_year_specific', instance_code=INSTANCE_CODE, api_year=2025)
     session = client.connect()
     assert(session)
+
+
+@responses.activate
+def test_multiple_tokens_when_no_cache(base_url_response):
+    """Test that two clients with no token cache will pull different tokens"""
+    responses.post(
+        f'{BASE_URL}/{INSTANCE_CODE}/oauth/token',
+        json={
+            "access_token": 'first',
+            "expires_in": 1800,
+            "token_type": "bearer"
+        },
+        match=[
+            matchers.header_matcher({"Authorization": BASIC_AUTH_HEADER})
+        ]
+    )
+    responses.post(
+        f'{BASE_URL}/{INSTANCE_CODE}/oauth/token',
+        json={
+            "access_token": 'second',
+            "expires_in": 1800,
+            "token_type": "bearer"
+        },
+        match=[
+            matchers.header_matcher({"Authorization": BASIC_AUTH_HEADER})
+        ]
+    )
+
+    client_1 = EdFiClient(BASE_URL, CLIENT_KEY, CLIENT_SECRET, api_mode='instance_year_specific', instance_code=INSTANCE_CODE, api_year=2025)
+    client_2 = EdFiClient(BASE_URL, CLIENT_KEY, CLIENT_SECRET, api_mode='instance_year_specific', instance_code=INSTANCE_CODE, api_year=2025)
+
+    assert(client_1.session.access_token == 'first')
+    assert(client_2.session.access_token == 'second')
 
